db4aee0f5d878b6a285d906e63ca412ad26b8116
Tanishq Vashishtha <vashishthatanishq64@gmail.com>
2025-08-24 23:14:52 +0530

    new commit

---
+++ Array_practice_level1/Factors.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int maxFactor = 10;
+        int[] factors = new int[maxFactor];
+        int index = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                if (index == maxFactor) {
+                    maxFactor *= 2;
+                    int[] temp = new int[maxFactor];
+                    System.arraycopy(factors, 0, temp, 0, factors.length);
+                    factors = temp;
+                }
+                factors[index++] = i;
+            }
+        }
+
+        System.out.println("Factors of " + number + ":");
+        for (int i = 0; i < index; i++) {
+            System.out.print(factors[i] + " ");
+        }
+
+        input.close();
+    }
+}
---
+++ Array_practice_level1/FizzBuzz.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+            return;
+        }
+
+        String[] results = new String[number + 1];
+        for (int i = 0; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0 && i != 0) {
+                results[i] = "FizzBuzz";
+            } else if (i % 3 == 0 && i != 0) {
+                results[i] = "Fizz";
+            } else if (i % 5 == 0 && i != 0) {
+                results[i] = "Buzz";
+            } else {
+                results[i] = String.valueOf(i);
+            }
+        }
+
+        for (int i = 0; i < results.length; i++) {
+            System.out.println("Position " + i + " = " + results[i]);
+        }
+
+        input.close();
+    }
+}
